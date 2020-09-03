4ccbc205240961b0045a07aafedb4c1acc478fbc
Sandeep M.V.N <sandeep.mvn@deccansoft.com>
2020-09-03 08:44:29 +0530

    html Helper example

--- ASPCoreAppUsingMVC/Controllers/HtmlHelperExampleController.cs
+++ ASPCoreAppUsingMVC/Controllers/HtmlHelperExampleController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPCoreAppUsingMVC.Controllers
@@ -9,6 +10,12 @@ namespace ASPCoreAppUsingMVC.Controllers
     public class HtmlHelperExampleController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection fc)
         {
             return View();
         }
