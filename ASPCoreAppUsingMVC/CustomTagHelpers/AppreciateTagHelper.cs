36b17429201015e7999db04d81acb1b395fec14e
Sandeep M.V.N <sandeep.mvn@deccansoft.com>
2020-09-14 10:56:28 +0530

    Custom Tag Helper

---
+++ ASPCoreAppUsingMVC/CustomTagHelpers/AppreciateTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ASPCoreAppUsingMVC.CustomTagHelpers
+{
+    [HtmlTargetElement("appreciated",TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class AppreciateTagHelper:TagHelper
+    {
+        public string PersonNameForAppreciation { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            // base.Process(context, output);
+            output.TagName = "p";
+            var appreciateMsg = "Great Work!" + ", " + PersonNameForAppreciation;
+            output.Content.SetContent(appreciateMsg);
+        }
+
+    }
+}
---
+++ ASPCoreAppUsingMVC/CustomTagHelpers/EmailTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ASPCoreAppUsingMVC.CustomTagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string EmailTo { get; set; }
+        const string domainName = "@deccansoft.com";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //base.Process(context, output);
+            output.TagName = "a";
+            output.Attributes.Add("href", $"mailto:{EmailTo}{domainName}");
+            output.Content.SetContent(EmailTo+ domainName);
+        }
+    }
+}
