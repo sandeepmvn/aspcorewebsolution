40ba1ea07780905e38df61b8854bec790f1c2ae8
Sandeep M.V.N <sandeep.mvn@deccansoft.com>
2020-09-22 08:40:50 +0530

    cookies Example

---
+++ ASPCoreAppUsingMVC/Controllers/StateManagementExamplesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+
+namespace ASPCoreAppUsingMVC.Controllers
+{
+    public class StateManagementExamplesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string cookieName, string cookievalue, bool chkIsPresistent, IFormCollection fc)
+        {
+            string data = string.Empty;
+
+            if (fc.ContainsKey("btnSubmit"))
+            {
+                CookieOptions options = new CookieOptions();
+                //Presistent Cookie
+                if (chkIsPresistent)
+                    options.Expires = DateTime.Now.AddDays(1);
+                Response.Cookies.Append(cookieName, cookievalue, options);
+            }
+            else if(fc.ContainsKey("btnReadCookies"))
+            {
+                foreach (var item in Request.Cookies)
+                {
+                    data += $" {item.Key} = {item.Value} ";
+                }
+            }
+            else if (fc.ContainsKey("btnGet"))
+            {
+                if (Request.Cookies[cookieName] != null)
+                {
+                   data = cookieName +" = "+Request.Cookies[cookieName];
+                }
+
+            }
+            else if(fc.ContainsKey("btnDelete"))
+            {
+                Response.Cookies.Delete(cookieName);
+            }
+
+            ViewBag.CookieData = data;
+            return View();
+        }
+    }
+}
--- ASPCoreAppUsingMVC/Startup.cs
+++ ASPCoreAppUsingMVC/Startup.cs
@@ -38,16 +38,16 @@ namespace ASPCoreAppUsingMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                //app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    //app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();//Https
             app.UseStaticFiles();
@@ -93,7 +93,7 @@ namespace ASPCoreAppUsingMVC
 
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller= ConfigExample}/{action=Index}/{id?}");
+                    pattern: "{controller= StateManagementExamples}/{action=Index}/{id?}");
             });
         }
     }
